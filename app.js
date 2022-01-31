40eae3d23af6bd010e5eec5b5a39f50cdaf09c46
sushant <densushant@gmail.com>
2022-01-31 13:16:15 +0530

    user done

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/users', require('./users/router'));
